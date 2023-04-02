289596175fa5895877b78dd43d1a256c360fdc49
Kevin M. Gill <kmsmgill@gmail.com>
2023-04-02 16:36:05 -0700

    Moved method cwd() from MRU, plus some tests

---
+++ tests/path.rs
@@ -0,0 +1,26 @@
+use sciimg::path;
+
+#[test]
+fn test_file_exists() {
+    assert!(path::file_exists("Cargo.toml"));
+}
+
+#[test]
+fn test_file_writable() {
+    assert!(path::file_writable("Cargo.toml"));
+}
+
+#[test]
+fn test_parent_exists() {
+    assert!(path::parent_exists("Cargo.toml"));
+}
+
+#[test]
+fn test_parent_writable() {
+    assert!(path::parent_writable("Cargo.toml"));
+}
+
+#[test]
+fn test_parent_exists_and_writable() {
+    assert!(path::parent_exists_and_writable("Cargo.toml"));
+}
