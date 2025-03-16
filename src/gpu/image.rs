3c0aec80d89582066e2d5cb5603bcdabb4283d18
Jer <alphastrata@gmail.com>
2025-03-16 12:41:57 +1100

    wip: GpuImage to sciimg::Image -> save_to_disk()

--- src/gpu/image.rs
+++ src/gpu/image.rs
@@ -85,74 +85,51 @@ impl GpuImage<Vec3> {
         }
     }
 
-    pub fn to_sciimg_rgb(&self) -> Image {
+    pub fn to_sciimg_rgb(&self) -> Result<Image> {
         let size = (self.width * self.height) as usize;
 
-        // Make band buffers
+        // Split Vec3 channels
         let mut red_buf = Vec::with_capacity(size);
         let mut green_buf = Vec::with_capacity(size);
         let mut blue_buf = Vec::with_capacity(size);
-
-        // Split out the Vec3 channels into separate bands
         for px in &self.data {
             red_buf.push(px.x);
             green_buf.push(px.y);
             blue_buf.push(px.z);
         }
 
-        // Wrap them in MaskedDnVec (assuming some float-based DnVec).
-        // In practice, adapt if your DnVec can be I16, etc.
-        let mask = MaskVec::new(size, false); // no masked-out pixels
-        let dn_red = MaskedDnVec {
-            vec: DnVec::F32(red_buf),
-            mask: mask.clone(),
-            null: 0.0,
-        };
-        let dn_green = MaskedDnVec {
-            vec: DnVec::F32(green_buf),
-            mask: mask.clone(),
-            null: 0.0,
-        };
-        let dn_blue = MaskedDnVec {
-            vec: DnVec::F32(blue_buf),
-            mask: mask,
-            null: 0.0,
-        };
+        let red_band = ImageBuffer::from_vec_as_mode(
+            &DnVec(red_buf),
+            self.width as usize,
+            self.height as usize,
+            enums::ImageMode::U16BIT, // or U8BIT, etc
+        )?;
 
-        let band_r = ImageBuffer {
-            buffer: dn_red,
-            width: self.width as usize,
-            height: self.height as usize,
-            empty: false,
-            mode: enums::ImageMode::U8BIT, // or whatever fits your data
-        };
-        let band_g = ImageBuffer {
-            buffer: dn_green,
-            width: self.width as usize,
-            height: self.height as usize,
-            empty: false,
-            mode: enums::ImageMode::U8BIT,
-        };
-        let band_b = ImageBuffer {
-            buffer: dn_blue,
-            width: self.width as usize,
-            height: self.height as usize,
-            empty: false,
-            mode: enums::ImageMode::U8BIT,
-        };
+        let green_band = ImageBuffer::from_vec_as_mode(
+            &DnVec(green_buf),
+            self.width as usize,
+            self.height as usize,
+            enums::ImageMode::U16BIT,
+        )?;
 
-        Image {
-            bands: vec![band_r, band_g, band_b],
+        let blue_band = ImageBuffer::from_vec_as_mode(
+            &DnVec(blue_buf),
+            self.width as usize,
+            self.height as usize,
+            enums::ImageMode::U16BIT,
+        )?;
+
+        Ok(Image {
+            bands: vec![red_band, green_band, blue_band],
             alpha: MaskVec::new(size, false),
             uses_alpha: false,
             width: self.width as usize,
             height: self.height as usize,
-            mode: enums::ImageMode::U8BIT,
+            mode: enums::ImageMode::U16BIT,
             empty: false,
-        }
+        })
     }
 }
-
 impl GpuImage<Vec4> {
     pub fn from_sciimg_with_alpha(img: &Image) -> Self {
         let size = img.width * img.height;
